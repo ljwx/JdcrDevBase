12e2732d74d121556e4f72c59760a9e389d6039f
9527 <1146699396@qq.com>
2026-06-13 15:27:47 +0800

    [feat]:新增定义,批量写入日志

--- jdcrbase/src/main/java/com/jdcr/jdcrbase/database/IDevBaseDatabaseLog.kt
+++ jdcrbase/src/main/java/com/jdcr/jdcrbase/database/IDevBaseDatabaseLog.kt
@@ -6,6 +6,8 @@ interface IDevBaseDatabaseLog {
 
     fun write(log: JdcrLogData)
 
+    fun write(logs: List<JdcrLogData>)
+
     fun read(startTs: Long, endTs: Long): List<JdcrLogData>
 
     fun readTag(tag: String, startTs: Long, endTs: Long): List<JdcrLogData>
