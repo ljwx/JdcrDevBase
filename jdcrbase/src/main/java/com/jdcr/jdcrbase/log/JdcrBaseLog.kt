ea4d4fb1c6d914a537a9b12ffc8f637f191ae92a
9527 <1146699396@qq.com>
2026-06-14 12:51:14 +0800

    [opt]:优化类定义

--- jdcrbase/src/main/java/com/jdcr/jdcrbase/log/JdcrBaseLog.kt
+++ jdcrbase/src/main/java/com/jdcr/jdcrbase/log/JdcrBaseLog.kt
@@ -11,38 +11,3 @@ data class JdcrLogData(
     val versionName: String,
     val extraData: String? = null
 )
-
-interface IJdcrLogBase {
-
-    fun enable(debugMode: Boolean)
-
-    fun v(msg: String?)
-
-    fun vF(feature: String, msg: String?)
-
-    fun vT(tag: String, msg: String?)
-
-    fun d(msg: String?, t: Throwable? = null)
-
-    fun dF(feature: String, msg: String?, t: Throwable? = null)
-
-    fun dT(tag: String, msg: String?, t: Throwable? = null)
-
-    fun i(msg: String?, t: Throwable? = null)
-
-    fun iF(feature: String, msg: String?, t: Throwable? = null)
-
-    fun iT(tag: String, msg: String?, t: Throwable? = null)
-
-    fun w(msg: String?, t: Throwable? = null)
-
-    fun wF(feature: String, msg: String?, t: Throwable? = null)
-
-    fun wT(tag: String, msg: String?, t: Throwable? = null)
-
-    fun e(msg: String?, t: Throwable?)
-
-    fun eF(feature: String, msg: String?, t: Throwable?)
-
-    fun eT(tag: String, msg: String?, t: Throwable?)
-}
